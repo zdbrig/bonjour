43225134059908da4a6fbbaa7f62972d19d3974c
Lance Ball <lball@redhat.com>
2016-12-07 09:09:42 -0500

    feat: First pass at swagger implementation

--- bonjour.js
+++ bonjour.js
@@ -14,59 +14,31 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// jshint esnext: true
-
 const express = require('express');
-const fs = require('fs');
 const session = require('express-session');
-const Keycloak = require('keycloak-connect');
-const {Tracer, ExplicitContext, BatchRecorder, ConsoleRecorder} = require('zipkin');
-const zipkinMiddleware = require('zipkin-instrumentation-express').expressMiddleware;
+const swagger = require('swagger-express');
+const zipkin = require('./lib/zipkin');
+const api = require('./lib/api');
 
-// chaining
-const roi = require('roi');
-const circuitBreaker = require('opossum');
-
-// circuit breaker
-const circuitOptions = {
-  maxFailures: 5,
-  timeout: 1000,
-  resetTimeout: 10000
-};
-const nextService = 'ola';
-const circuit = circuitBreaker(roi.get, circuitOptions);
-circuit.fallback(() => (`The ${nextService} service is currently unavailable.`));
-
-const chainingOptions = {
-  endpoint: `http://${nextService}:8080/api/${nextService}-chaining`
-};
-
-const ctxImpl = new ExplicitContext();
-const {HttpLogger} = require('zipkin-transport-http');
-const os = require('os');
 const app = express();
 
-let recorder;
-if (process.env.ZIPKIN_SERVER_URL === undefined) {
-  console.log('No ZIPKIN_SERVER_URL defined. Printing zipkin traces to console.');
-  recorder = new ConsoleRecorder();
-} else {
-  recorder = new BatchRecorder({
-    logger: new HttpLogger({
-      endpoint: process.env.ZIPKIN_SERVER_URL + '/api/v1/spans'
-    })
-  });
-}
-
-const tracer = new Tracer({
-  recorder,
-  ctxImpl // this would typically be a CLSContext or ExplicitContext
-});
+app.use(swagger.init(app, {
+  apiVersion: '1.0',
+  swaggerVersion: '1.0',
+  basePath: 'http://localhost:3000',
+  swaggerURL: '/',
+  swaggerJSON: '/api-docs.json',
+  swaggerUI: './public/swagger',
+  info: {
+    title: 'Bonjour microservices application',
+    description: 'Operations that can be invoked in the bonjour microservices'
+  },
+  apis: ['./lib/api.js']
+}));
 
 // Create a session-store to be used by both the express-session
 // middleware and the keycloak middleware.
 const memoryStore = new session.MemoryStore();
-
 app.use(session({
   secret: 'mySecret',
   resave: false,
@@ -74,45 +46,26 @@ app.use(session({
   store: memoryStore
 }));
 
-app.use(zipkinMiddleware({
-  tracer,
-  serviceName: 'bonjour' // name of this application
-}));
+// Use our zipkin integration
+app.use(zipkin);
 
-// Configure keycloak based on keycloak.json and the KEYCLOAK_AUTH_SERVER_URL env var
-const customKeyCloakConfig = JSON.parse(fs.readFileSync('keycloak.json').toString());
-customKeyCloakConfig.authServerUrl = process.env.KEYCLOAK_AUTH_SERVER_URL;
+/**
+ * @swagger
+ * resourcePath: /api
+ * prettyPrint: true
+ */
 
-const keycloak = new Keycloak({ scope: 'USERS', store: memoryStore }, customKeyCloakConfig);
+app.use('/api', api.secure(memoryStore));
 
-app.use(keycloak.middleware({ logout: '/api/logout' }));
-
+// Enable CORS
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 
+// default route (should be swagger)
 app.get('/', (req, res) => res.send('Logged out'));
-
-const sayBonjour = () => `Bonjour de ${os.hostname()}`;
-
-app.get('/api/bonjour', (req, resp) => resp.send(sayBonjour()));
-
-app.get('/api/bonjour-secured', keycloak.protect(),
-  (req, resp) => resp.send(`This is a Secured resource. You're logged as ${req.kauth.grant.access_token.content.name}`));
-
-app.get('/api/bonjour-chaining', (req, resp) =>
-  circuit.fire(chainingOptions).then((response) => {
-    resp.set('Access-Control-Allow-Origin', '*');
-    resp.send(response);
-  }).catch((e) => resp.send(e))
-);
-
-app.get('/api/health', (req, resp) => {
-  resp.set('Access-Control-Allow-Origin', '*');
-  resp.send('I am ok');
-});
 
 const server = app.listen(8080, '0.0.0.0', () => {
   const host = server.address().address;
